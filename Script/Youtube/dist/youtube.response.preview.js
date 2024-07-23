19373eff6a8c036a3fdbd9e8ad1cbc70fd8f0e77
space <winlinkersa@gmail.com>
2024-07-23 13:31:34 +0800

    Update youtube.response.preview.js

--- Script/Youtube/dist/youtube.response.preview.js
+++ Script/Youtube/dist/youtube.response.preview.js
@@ -2855,15 +2855,17 @@
         }
         decodeArgument() {
             let e = {
-                    lyricLang: "\u6B4C\u8BCD\u7FFB\u8BD1\u8BED\u8A00",
-                    captionLang: "\u5B57\u5E55\u7FFB\u8BD1\u8BED\u8A00",
+                    //lyricLang: "\u6B4C\u8BCD\u7FFB\u8BD1\u8BED\u8A00",
+                    //captionLang: "\u5B57\u5E55\u7FFB\u8BD1\u8BED\u8A00",
+                    lyricLang: "English",
+                    captionLang: "English",
                     blockUpload: "\u5C4F\u853D\u4E0A\u4F20\u6309\u94AE",
                     blockImmersive: "\u5C4F\u853D\u9009\u6BB5\u6309\u94AE",
                     debug: "\u542F\u52A8\u8C03\u8BD5\u6A21\u5F0F"
                 },
                 n = {
-                    lyricLang: "zh-Hans",
-                    captionLang: "zh-Hans",
+                    lyricLang: "en",
+                    captionLang: "en",
                     blockUpload: !0,
                     blockImmersive: !0,
                     debug: !1
@@ -2977,7 +2979,7 @@
     }
 
     function Gt(t, e) {
-        return `https://translate.google.com/translate_a/single?client=gtx&sl=auto&tl=${e}&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&source=bh&ssel=0&tsel=0&kc=1&tk=${ir(t)}&q=${encodeURIComponent(t)}`
+        return `https://translate.google.com/translate_a/single?client=gtx&sl=auto&tl=${e}&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&source=bh&ssel=0&tsel=0&kc=1&tk=${ir(t)}&q=${encodeURIComponent(t)}`
     }
     var X = class extends D {
             constructor(e = Rt, n = "Browse") {
